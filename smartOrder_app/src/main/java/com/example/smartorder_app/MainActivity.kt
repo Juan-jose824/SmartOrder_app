8b771ac2ef24c86da4979e75db7c2f1e1058ed51
Francisco <gamercrepy8@gmail.com>
2025-08-11 00:56:55 -0600

    appMovil login update

--- smartOrder_app/src/main/java/com/example/smartorder_app/MainActivity.kt
+++ smartOrder_app/src/main/java/com/example/smartorder_app/MainActivity.kt
@@ -1,25 +1,45 @@
 package com.example.smartorder_app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.smartorder_app.ui.theme.SmartOrderTheme
+import utils.Cellphone
+import utils.InternalData
+import utils.Name
+import utils.UserConfig
 
 class MainActivity : ComponentActivity() {
+    private lateinit var config: UserConfig
+    private lateinit var localStorage: InternalData
+    private val interval = 30_000L
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
+        localStorage = InternalData(this)
+        config = localStorage.getData() ?:
+        UserConfig(Name("Usuario", "", ""), "", Cellphone("", ""), "0", "", false)
 
-        setContentView(R.layout.login)
+        if (config.isLoggedIn == true) {
+            setContentView(R.layout.main)
+        } else {
+            setContentView(R.layout.login)
+
+            val singingButton : Button =findViewById(R.id.singinButton)
+
+            singingButton.setOnClickListener {
+
+            }
+        }
     }
 }
 
